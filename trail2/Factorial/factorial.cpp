2158d73f1b2ea64aa3acb025f8b8b0626a9e31d9
jaesun0511 <jaesun0511@gmail.com>
2026-05-26 13:22:25 +0900

    [Passed] Factorial (2ms, 0MB)

---
+++ trail2/Factorial/factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int fac(int n){
+    if(n==1){
+        return 1;
+    }
+
+    return fac(n-1)*n;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    cout<<fac(n);
+    // Please write your code here.
+    return 0;
+}
