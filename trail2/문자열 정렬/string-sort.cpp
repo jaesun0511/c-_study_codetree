17ebfa1410d17fe8147d3f9371db0c67f1187f7b
jaesun0511 <jaesun0511@gmail.com>
2026-05-26 14:51:06 +0900

    [Passed] 문자열 정렬 (2ms, 0MB)

---
+++ trail2/문자열 정렬/string-sort.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    string str;
+    cin>>str;
+
+    sort(str.begin(), str.end());
+    cout<<str;
+    // Please write your code here.
+    return 0;
+}
