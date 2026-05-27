5e1b48af9e9e455f7b382ef8c5092613106dd406
jaesun0511 <jaesun0511@gmail.com>
2026-05-27 21:04:32 +0900

    [Passed] 단어 정렬 (2ms, 0MB)

---
+++ trail2/단어 정렬/sorting-words.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    string arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    sort(arr, arr+n);
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<endl;
+    }
+
+    // Please write your code here.
+    return 0;
+}
